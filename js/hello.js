b4c60f0af097a04c7bd86d3b722cf395393c5243
ubermensch <ubermensch@ubermenschs-MacBook-Air.local>
2025-04-14 08:22:31 +0530

    added 2 days of work

---
+++ js/hello.js
@@ -0,0 +1,17 @@
+// chai aur furniture
+function printchai() {
+  console.log('hello from chai');
+}
+
+function bringbruuush(itne) {
+  console.log(`hanji le aaya ${itne} brush`);
+}
+function addtwo(ek, two) {
+  return ek + two;
+}
+bringbruuush(4);
+
+printchai(); // A JI SUNTE HO
+console.log(addtwo(2, 3)); // bolna pyaar se padega
+
+// arre bahut basic hai aage chal bhai
---
+++ js/ooutfit.js
@@ -0,0 +1,9 @@
+let weather = 'rainy';
+
+if (weather === 'rainy') {
+  console.log('bhai chatri leke jaana');
+} else if (weather === 'cloudy') {
+  console.log('jacket pehen le bhai');
+} else {
+  console.log('mauj maar');
+}
