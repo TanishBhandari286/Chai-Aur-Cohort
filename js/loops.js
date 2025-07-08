8afee90ed442a3b820fea0733b12d8dac951a257
tanishbhandari286 <tanishbhandari91@gmail.com>
2025-07-08 18:21:07 +0530

    late to the party

--- js/loops.js
+++
@@ -1,5 +0,0 @@
-let teas = ['masala', 'ginger', 'oolong', 'orange', 'lemon'];
-
-for (let h = 0; h < teas.length; h++) {
-  console.log(`tea at index ${h}: ${teas[h]}`);
-}
--- js/ooutfit.js
+++
@@ -1,9 +0,0 @@
-let weather = 'rainy';
-
-if (weather === 'rainy') {
-  console.log('bhai chatri leke jaana');
-} else if (weather === 'cloudy') {
-  console.log('jacket pehen le bhai');
-} else {
-  console.log('mauj maar');
-}
