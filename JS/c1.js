5fc4d49bf1ffca2e4cf3197e9d80fbf4420cd925
tanishbhandari286 <tanishbhandari91@gmail.com>
2025-07-09 11:57:07 +0530

    basics of js

---
+++ JS/c1.js
@@ -0,0 +1,17 @@
+function addtwo(tray1,tray2){
+    return tray1+tray2
+}
+
+console.log(addtwo(2,90));
+
+function multitwo(tray1,tray2){
+    return tray1*tray2
+}
+
+console.log(multitwo(2,90));
+
+function divtwo(tray1,tray2){
+    return tray2/tray1
+}
+
+console.log(divtwo(2,90));
---
+++ JS/hello.js
@@ -0,0 +1,27 @@
+console.log("hello from chai")
+
+function printchai() {
+
+  console.log("hello chai")
+}
+
+function bringbrush(number){
+  console.log("brush aagye", number);
+  
+}
+function bringbrush(number){
+  console.log(`hanji , le aaya ${number} brush`);
+  
+}
+
+function addtwo(a,b){
+  return a+b;
+}
+bringbrush(4)
+printchai();
+
+console.log(addtwo(2,3));
+
+
+
+
